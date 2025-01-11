bf215dbe58c00e4b79036f3545625209976c1990
Giovanni Cortazar <manuelgiozar04@gmail.com>
2025-01-11 02:22:49 -0600

    [Server] Update: Se actualiza contrato de productos

--- petopia-server/src/products/models/product.model.ts
+++ petopia-server/src/products/models/product.model.ts
@@ -3,9 +3,11 @@ import { CreateProductDto } from '../interfaces/product.dto'
 
 const productSchema = new Schema<CreateProductDto>({
   name: { type: String, required: true },
+  description: { type: String, required: true },
   price: { type: Number, required: true },
   category: { type: String, required: true },
-  inStock: { type: Boolean, required: true },
+  stock: { type: Number, required: true },
+  species: { type: [String], required: true },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now }
 },{ timestamps: true })
